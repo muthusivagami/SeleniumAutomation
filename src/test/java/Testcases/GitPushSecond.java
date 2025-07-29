f94ce9ed18e2051a022cf5224b0858d03ad761e9
Muthusivagami Rajendran <r.muthusivagami@gmail.com>
2025-07-29 08:43:47 +0530

    Merge branch 'master' of https://github.com/muthusivagami/SeleniumAutomation.git
    Signed-off-by: Muthusivagami Rajendran <r.muthusivagami@gmail.com>

---
+++ src/test/java/Testcases/GitPushSecond.java
@@ -0,0 +1,10 @@
+package Testcases;
+
+public class GitPushSecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("second time afterpull");
+	}
+
+}
